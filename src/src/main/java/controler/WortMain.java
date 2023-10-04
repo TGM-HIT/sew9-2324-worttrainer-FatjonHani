b6f0cb7f2a1d77d505a680ddcb559d365736a1a1
Fatjon Hani <fhani@student.tgm.ac.at>
2023-10-04 14:36:49 +0200

    Laden und Speichern in Control ausgeführt

--- src/src/main/java/controler/WortMain.java
+++ src/src/main/java/controler/WortMain.java
@@ -2,6 +2,8 @@ package controler;
 
 import model.Rechtschreibtrainer;
 import model.WortEintrag;
+import model.WortSpeicher;
+import org.json.JSONObject;
 import view.Anzeige;
 
 import java.net.MalformedURLException;
@@ -10,7 +12,7 @@ import java.util.ArrayList;
 
 public class WortMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
                     // rechtrainer fragen nach zuffäligem Worteinträgen und dann anzeigen
         try {
             URL url = new URL("https://static.nationalgeographic.de/files/styles/image_3200/public/01-domesticated-dog.jpg?w=400&h=400&q=75");
@@ -33,6 +35,21 @@ public class WortMain {
 
         }catch (MalformedURLException e){
             e.printStackTrace();
+        }
+        // Beispiel für die Verwendung der WortSpeicher-Klasse
+        WortSpeicher wortSpeicher = new WortSpeicher();
+        wortSpeicher.addWordPair("Hund", new URL("https://static.nationalgeographic.de/files/styles/image_3200/public/01-domesticated-dog.jpg?w=400&h=400&q=75"));
+        wortSpeicher.addWordPair("Katze", new URL("https://api.ardmediathek.de/image-service/images/urn:ard:image:3dab66faa8140d8b?w=448&ch=9e935de585dfb889"));
+        wortSpeicher.saveSession("wortSpeicher.json");
+
+        // Laden der gespeicherten WortSpeicher
+        WortSpeicher loadedWortSpeicher = new WortSpeicher();
+        loadedWortSpeicher.loadSession("wortSpeicher.json");
+
+        // Verwendung der geladenen WortSpeicher
+        JSONObject currentPair = loadedWortSpeicher.getCurrentWordPair();
+        if (currentPair != null) {
+            System.out.println("Aktuelles Paar: " + currentPair);
         }
     }
 }
--- src/src/main/java/model/WortSpeicher.java
+++ src/src/main/java/model/WortSpeicher.java
@@ -63,21 +63,4 @@ public class WortSpeicher {
         }
     }
 
-    public static void main(String[] args) throws MalformedURLException {
-        // Beispiel für die Verwendung der WortSpeicher-Klasse
-        WortSpeicher wortSpeicher = new WortSpeicher();
-        wortSpeicher.addWordPair("Hund", new URL("https://static.nationalgeographic.de/files/styles/image_3200/public/01-domesticated-dog.jpg?w=400&h=400&q=75"));
-        wortSpeicher.addWordPair("Katze", new URL("https://api.ardmediathek.de/image-service/images/urn:ard:image:3dab66faa8140d8b?w=448&ch=9e935de585dfb889"));
-        wortSpeicher.saveSession("wortSpeicher.json");
-
-        // Laden der gespeicherten WortSpeicher
-        WortSpeicher loadedWortSpeicher = new WortSpeicher();
-        loadedWortSpeicher.loadSession("wortSpeicher.json");
-
-        // Beispiel für die Verwendung der geladenen WortSpeicher
-        JSONObject currentPair = loadedWortSpeicher.getCurrentWordPair();
-        if (currentPair != null) {
-            System.out.println("Aktuelles Paar: " + currentPair);
-        }
-    }
 }
