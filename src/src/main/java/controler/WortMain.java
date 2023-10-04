655294d58080db5dd09e66d07fb66c6c22da0140
Fatjon Hani <fhani@student.tgm.ac.at>
2023-10-04 14:50:34 +0200

    Session class

--- src/src/main/java/controler/WortMain.java
+++ src/src/main/java/controler/WortMain.java
@@ -40,11 +40,11 @@ public class WortMain {
         WortSpeicher wortSpeicher = new WortSpeicher();
         wortSpeicher.addWordPair("Hund", new URL("https://static.nationalgeographic.de/files/styles/image_3200/public/01-domesticated-dog.jpg?w=400&h=400&q=75"));
         wortSpeicher.addWordPair("Katze", new URL("https://api.ardmediathek.de/image-service/images/urn:ard:image:3dab66faa8140d8b?w=448&ch=9e935de585dfb889"));
-        wortSpeicher.saveSession("wortSpeicher.json");
+        wortSpeicher.saveSession("C:\\Users\fatjo\\OneDrive\\Desktop\5CHIT\\SEW\\Worttrainer.txt");
 
         // Laden der gespeicherten WortSpeicher
         WortSpeicher loadedWortSpeicher = new WortSpeicher();
-        loadedWortSpeicher.loadSession("wortSpeicher.json");
+        loadedWortSpeicher.loadSession("C:\\Users\\fatjo\\OneDrive\\Desktop\\5CHIT\\SEW\\Worttrainer.txt");
 
         // Verwendung der geladenen WortSpeicher
         JSONObject currentPair = loadedWortSpeicher.getCurrentWordPair();
